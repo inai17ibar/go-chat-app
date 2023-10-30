60b53b7f82d7aef12794ac996e527f0433f51767
Soichiro Inatani <inai17ibar@gmail.com>
2023-10-31 00:19:47 +0900

    chatアプリケーションの雛形を作った。ディレクトリ構成は変えたほうがいいかも

--- main.go
+++ main.go
@@ -3,23 +3,75 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-		<html>
-			<head>
-				<title>Chat</title>
-			</head>
-			<body>
-			Let's try chat!
-			</body>
-		</html>
-		`))
-	})
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal("ListenAndServe:", err)
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+
+type Message struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
+func handleConnections(c *gin.Context) {
+	w := c.Writer
+	r := c.Request
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatalf("error: %v", err)
 	}
+	defer ws.Close()
+	clients[ws] = true
+
+	for {
+		var msg Message
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(clients, ws)
+			break
+		}
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
+
+func main() {
+	r := gin.Default()
+
+	// CORSミドルウェアの設定
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"GET", "POST"}
+	r.Use(cors.New(config))
+
+	r.GET("/ws", handleConnections)
+
+	go handleMessages()
+
+	r.Run(":8000")
 }
